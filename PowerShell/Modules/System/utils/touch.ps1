21b4804145678a442f05bc0fa315b88bd3136ff5
Dot Choco <chocoretostonus@gmail.com>
2025-10-04 18:05:12 -0600

    f: menor update

--- PowerShell/Modules/System/utils/touch.ps1
+++ PowerShell/Modules/System/utils/touch.ps1
@@ -4,9 +4,7 @@ function touch {
     [string]$dir_file,
     [string]$data
   )
-
-  $final_dir = (Get-Location).Path + $dir_file.Substring(1)
-  echo($data) >> $dir_file
+  Write-Output($data) >> $dir_file
 }
 
 
